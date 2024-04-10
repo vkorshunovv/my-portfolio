26115f7ecad194bcf1d1a8e61fe38a6f96a7210a
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-04-10 16:08:52 +0200

    almost finished Project Cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,98 +1,87 @@
-
-
-export default function ProjectCard({isDarkMode, img_dark, img_light}) {
-    return (
-        <>
-        <section className="col-start-1 lg:col-start-2 col-end-5 lg:col-end-12 row-span-1 block lg:flex justify-between items-center pt-20 m-4 lg:m-0 lg:pt-0 space-y-5 lg:space-y-0 ">
-          <div
-            className={`w-full lg:basis-5/12 bg-gradient-to-l rounded-2xl lg:mr-6 size-24 lg:size-56 reveal fade-bottom ${
-              isDarkMode
-                ? "from-cyan-950 to-emerald-950"
-                : "from-red-300 to-rose-200"
-            }`}
-          >
-            <div className="h-full p-4 flex flex-col justify-between">
-              <span
-                className={`${
-                  isDarkMode ? "text-teal-300" : "text-red-900"
-                } text-xl font-medium `}
+export default function ProjectCard({ isDarkMode, main }) {
+  return (
+    <>
+      <section className="col-start-1 lg:col-start-2 col-end-5 lg:col-end-12 row-span-1 block lg:flex justify-between items-center pt-20 m-4 lg:m-0 lg:pt-0 space-y-5 lg:space-y-0 ">
+        <div
+          className={`w-full lg:basis-5/12 bg-gradient-to-l rounded-2xl lg:mr-6 size-24 lg:size-56 reveal fade-bottom ${
+            isDarkMode
+              ? "from-cyan-950 to-emerald-950"
+              : "from-red-300 to-rose-200"
+          }`}
+        >
+          <div className="h-full p-4 flex flex-col justify-between">
+            <span
+              className={`${
+                isDarkMode ? "text-teal-300" : "text-red-900"
+              } text-xl font-medium `}
+            >
+              {main.title}
+            </span>
+            <p
+              className={`text-pretty font-normal text-sm leading-normal ${
+                isDarkMode ? "text-slate-400" : "text-slate-800"
+              }`}
+            >
+              {main.description}
+            </p>
+            <div className="flex flex-row space-x-3">
+              <div
+                className={` rounded-full ${
+                  isDarkMode ? "bg-teal-400/10" : "bg-red-600/10"
+                } `}
               >
-                Frontend Developer Portfolio
-              </span>
-              <p
-                className={`text-pretty font-normal text-sm leading-normal ${
-                  isDarkMode ? "text-slate-400" : "text-slate-800"
-                }`}
+                <p
+                  className={`px-2 py-1 text-xs font-medium ${
+                    isDarkMode ? "text-teal-300" : "text-red-800"
+                  }`}
+                >
+                  {main.assets && main.assets.length && main.assets[0]}
+                </p>
+              </div>
+              <div
+                className={` rounded-full ${
+                  isDarkMode ? "bg-teal-400/10" : "bg-red-600/10"
+                } `}
               >
-                My portfolio website is more than just a digital resume — it's a
-                dynamic platform where I showcase my creativity, skills, and
-                projects in web development. With a modern, interactive and
-                responsive design, website is accessible across devices and
-                ensures a seamless user experience for anyone interested in
-                exploring my work and connecting with me.
-              </p>
-              <div className="flex flex-row space-x-3">
-                <div
-                  className={` rounded-full ${
-                    isDarkMode ? "bg-teal-400/10" : "bg-red-600/10"
-                  } `}
+                <p
+                  className={`px-2 py-1 text-xs font-medium ${
+                    isDarkMode ? "text-teal-300" : "text-red-800"
+                  }`}
                 >
-                  <p
-                    className={`px-2 py-1 text-xs font-medium ${
-                      isDarkMode ? "text-teal-300" : "text-red-800"
-                    }`}
-                  >
-                    React
-                  </p>
-                </div>
-                <div
-                  className={` rounded-full ${
-                    isDarkMode ? "bg-teal-400/10" : "bg-red-600/10"
-                  } `}
+                  {main.assets && main.assets.length && main.assets[1]}
+                </p>
+              </div>
+              <div
+                className={` rounded-full ${
+                  isDarkMode ? "bg-teal-400/10" : "bg-red-600/10"
+                } `}
+              >
+                <p
+                  className={`px-2 py-1 text-xs font-medium ${
+                    isDarkMode ? "text-teal-300" : "text-red-800"
+                  }`}
                 >
-                  <p
-                    className={`px-2 py-1 text-xs font-medium ${
-                      isDarkMode ? "text-teal-300" : "text-red-800"
-                    }`}
-                  >
-                    Tailwind
-                  </p>
-                </div>
-                <div
-                  className={` rounded-full ${
-                    isDarkMode ? "bg-teal-400/10" : "bg-red-600/10"
-                  } `}
-                >
-                  <p
-                    className={`px-2 py-1 text-xs font-medium ${
-                      isDarkMode ? "text-teal-300" : "text-red-800"
-                    }`}
-                  >
-                    Netlify
-                  </p>
-                </div>
+                  {main.assets && main.assets.length && main.assets[2]}
+                </p>
               </div>
             </div>
           </div>
-          <div className="lg:basis-7/12 lg:ml-6 size-48 lg:size-96 reveal fade-bottom rounded-2xl ">
-            <a
-              href="/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="size-full"
-            >
-              <img
-                src={isDarkMode ? img_dark : img_light}
-                alt="Portfolio Web Site"
-                className={`rounded-2xl hover:scale-105 ease duration-300 w-full h-full grayscale hover:grayscale-0 hover:shadow-2xl border ${
-                  isDarkMode
-                    ? "hover:shadow-teal-950 border-teal-500/30 "
-                    : "border-teal-100/60 "
-                }`}
-              />
-            </a>
-          </div>
-        </section>
-        </>
-    )
+        </div>
+        <div className="lg:basis-7/12 lg:ml-6 size-48 lg:size-96 reveal fade-bottom rounded-2xl ">
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="size-full"
+          >
+            <img
+              src={isDarkMode ? main.img_dark : main.img_light}
+              alt={main.title}
+              className="rounded-2xl hover:scale-105 ease duration-300 w-full h-full grayscale hover:grayscale-0 hover:shadow-2xl"
+            />
+          </a>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,39 @@ import ProjectCard from "./ProjectCard";
 
 import portfolio_dark from "../assets/projects/portfolio_dark.png";
 import portfolio_light from "../assets/projects/portfolio_light.png";
+import guauleria_v1_img from "../assets/projects/guauleria_v1.png";
+import guauleria_v2_img from "../assets/projects/guauleria_v2.png";
 
 export default function Projects({ isDarkMode }) {
+  const portfolio = {
+    title: "Frontend Developer Portfolio",
+    description: `My portfolio website is more than just a digital resume — it's a
+dynamic platform where I showcase my creativity, skills, and
+projects in web development. With a modern, interactive and
+responsive design, website is accessible across devices and
+ensures a seamless user experience for anyone interested in
+exploring my work and connecting with me.`,
+    assets: ["React", "Tailwind", "Netlify"],
+    img_light: portfolio_light,
+    img_dark: portfolio_dark,
+  };
+
+  const guauleria_v1 = {
+    title: "Guauleria Web Site (v1)",
+    description: `Guauleria is your go-to destination for all things pet-related, offering a comprehensive marketplace and insurance platform tailored to meet the needs of pet owners worldwide. Developed with cutting-edge technology and fully-responsive design, Guauleria ensures that pet owners can access its features seamlessly across all devices.`,
+    assets: ["React", "Spring WebFlux", "CSS"],
+    img_light: guauleria_v1_img,
+    img_dark: guauleria_v1_img,
+  };
+
+  const guauleria_v2 = {
+    title: "Guauleria Web Site (v2)",
+    description: `Guauleria's second iteration builds upon its foundation as the ultimate pet-centric destination. This enhanced version incorporates feedback from pet owners, resulting in a refined user experience. With improved functionality, the site continues to be the go-to choice for pet owners seeking convenience, reliability, and unparalleled service.`,
+    assets: ["React", "Spring WebFlux", "CSS"],
+    img_light: guauleria_v2_img,
+    img_dark: guauleria_v2_img,
+  };
+
   return (
     <div
       id="projects"
@@ -19,9 +50,9 @@ export default function Projects({ isDarkMode }) {
         </div>
       </div>
       <div className="grid grid-cols-4 lg:grid-cols-12 lg:grid-rows-3 space-y-6 lg:space-y-12 pb-28">
-        <ProjectCard isDarkMode={isDarkMode} img_light={portfolio_light} img_dark={portfolio_dark}/>
-        <ProjectCard isDarkMode={isDarkMode} img_light={portfolio_light} img_dark={portfolio_dark}/>
-        <ProjectCard isDarkMode={isDarkMode} img_light={portfolio_light} img_dark={portfolio_dark}/>
+        <ProjectCard isDarkMode={isDarkMode} main={portfolio} />
+        <ProjectCard isDarkMode={isDarkMode} main={guauleria_v1} />
+        <ProjectCard isDarkMode={isDarkMode} main={guauleria_v2} />
       </div>
     </div>
   );
